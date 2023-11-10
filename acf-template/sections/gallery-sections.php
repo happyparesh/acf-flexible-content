5465b987b718e6f5e679ebd5667f95259030d0c2
Paresh Patel <70636561+happyparesh@users.noreply.github.com>
2023-11-10 11:23:13 +0530

    Update gallery-sections.php

--- acf-template/sections/gallery-sections.php
+++ acf-template/sections/gallery-sections.php
@@ -1,1 +1,10 @@
 <?php 
+if( have_rows('images') ):
+    echo '<ul>';
+    // loop through the rows of data
+      while ( have_rows('images') ) : the_row();
+        $image = get_sub_field('image');
+        echo '<li><img src="' . $image['url'] . '" alt="' . $image['alt'] . '" /></li>';
+      endwhile;
+    echo '</ul>';
+  endif;
